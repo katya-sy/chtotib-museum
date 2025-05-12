04816040d9b1c53519350e56a41237382a9fb17c
The73756 <the73756the@gmail.com>
2025-05-13 02:22:38 +0900

    add ckeditor styles

--- chtotib_museum/settings.py
+++ chtotib_museum/settings.py
@@ -42,9 +42,23 @@ INSTALLED_APPS = [
 
 CKEDITOR_5_CONFIGS = {
     'default': {
-        'toolbar': ['heading', '|', 'bold', 'italic', 'link',
+        'toolbar': ['heading', '|', 'bold', 'italic', 'underline', 'link',
                     'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
         'language': 'ru',
+        'contentsCss': ['/static/style.css'],
+    },
+    'heading': {
+        'options': [
+            {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
+            {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
+            {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
+            {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'},
+        ]
+    },
+    'htmlSupport': {
+        'allow': [
+            {'name': '/*/', 'attributes': True, 'classes': True, 'styles': True}
+        ]
     },
 }
 
