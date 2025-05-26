ae033b1eb3c5fec3f3670deea4d9627e1bf70f2d
The73756 <the73756the@gmail.com>
2025-05-26 18:58:53 +0900

    fix masonry (maybe)

--- static/js/masonry-init.js
+++ static/js/masonry-init.js
@@ -1,15 +1,17 @@
-const elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-});
+window.addEventListener('load', () => {
+  const elem = document.querySelector('.grid');
+  var msnry = new Masonry( elem, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+  });
 
-var msnry = new Masonry( '.masonry', {
-  horizontalOrder: true,
-  percentPosition: true,
-});
+  var msnry = new Masonry( '.masonry', {
+    horizontalOrder: true,
+    percentPosition: true,
+  });
 
-var msnry = new Masonry( '.gallery', {
-  // horizontalOrder: true,
-  percentPosition: true,
-});
+  var msnry = new Masonry( '.gallery', {
+    // horizontalOrder: true,
+    percentPosition: true,
+  });
+})
