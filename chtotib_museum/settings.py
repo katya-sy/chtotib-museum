b175508175c78d304d29798485a5aa7eaf734a5f
The73756 <the73756the@gmail.com>
2025-05-29 03:41:30 +0900

    add deleting page and login

--- chtotib_museum/settings.py
+++ chtotib_museum/settings.py
@@ -171,6 +171,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGOUT_REDIRECT_URL = '/admin/login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
